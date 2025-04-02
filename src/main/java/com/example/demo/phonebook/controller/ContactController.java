6f02547ca79929301fca3f9a6156d496555c111e
Harry Xu <harry.xv@gmail.com>
2025-04-02 14:47:52 -0400

    remove multipe Constructor without Autowired annotation testing

--- src/main/java/com/example/demo/phonebook/controller/ContactController.java
+++ src/main/java/com/example/demo/phonebook/controller/ContactController.java
@@ -2,13 +2,12 @@ package com.example.demo.phonebook.controller;
 
 import com.example.demo.phonebook.model.Contact;
 import com.example.demo.phonebook.service.ContactService;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
 
 import java.util.List;
 import java.util.Optional;
